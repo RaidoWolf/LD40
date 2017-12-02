c8cda54de83502d848d1db5faf791230f034ab28
Alexander Barber <dangerbarber@gmail.com>
2017-12-01 20:13:24 -0700

    Here we go! Got build system up and working.

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "config.h"
+
+int main (int argc, const char* argv[]) {
+
+    std::cout << "Here ya go, this is a string for you." << std::endl;
+
+    exit(EXIT_SUCCESS);
+    return 0;
+
+}
