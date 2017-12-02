71e59981204934a3a1209a89c76ec74dde181c37
Alexander Barber <dangerbarber@gmail.com>
2017-12-01 20:24:38 -0700

    Added a game window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include <string>
+#include <SFML/Graphics.hpp>
 #include "config.h"
 
 int main (int argc, const char* argv[]) {
 
-    std::cout << "Here ya go, this is a string for you." << std::endl;
+    // create game window
+    sf::Window window(
+        sf::VideoMode(800, 600),
+        "LD40"
+    );
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.display();
+    }
 
     exit(EXIT_SUCCESS);
     return 0;
